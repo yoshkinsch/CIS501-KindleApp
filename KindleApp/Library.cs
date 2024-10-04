a2f238273ec1945031334a5992a9eaf3200200ea
Jackson Seim <jseim4699@gmail.com>
2024-10-04 11:55:29 -0500

    hard coded the data

--- KindleApp/Library.cs
+++ KindleApp/Library.cs
@@ -8,6 +8,20 @@ namespace KindleApp
 {
     public class Library
     {
-        private Dictionary<int, Book> cloudBooks;
+        public Library()
+        {
+            Book JorgeVERYfunny = new Book("JorgeVERYfunny", "Jorge", 1, 500);
+            Book JorgeMAYBEfunny = new Book("JorgeMAYBEfunny", "Jorge 2", 2, 320);
+            Book JorgeNOTfunny = new Book("JorgeNOTfunny", "Jorge 3", 3, 420);
+            cloudBooks.Add(JorgeVERYfunny.ID, JorgeVERYfunny);
+            cloudBooks.Add(JorgeMAYBEfunny.ID, JorgeMAYBEfunny);
+            cloudBooks.Add(JorgeNOTfunny.ID, JorgeNOTfunny);
+            Book JorgeSuperFunny = new Book("Jorge Super Funny", "jorge the best", 12345678, 999);
+            Book JorgeClassAwesome = new Book("Jorge Class The Best", "jorge the best", 1234, 55);
+            cloudBooks.Add(JorgeClassAwesome.ID, JorgeClassAwesome);
+            cloudBooks.Add(JorgeSuperFunny.ID, JorgeSuperFunny);
+        }
+
+        private Dictionary<int, Book> cloudBooks = new Dictionary<int, Book>();
     }
 }
